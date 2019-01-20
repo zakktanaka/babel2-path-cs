9138161af467a38536df6eda6a82e3dfdb349ffa
SS <zakktanaka@hotmail.com>
2019-01-20 20:09:31 +0900

    babal directory

---
+++ BabelPath/BabelPath/Utilities/Path/BabelDirectory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel
+{
+    using WPath = System.IO.Path;
+
+    public class BabelDirectory
+    {
+        private Uri uri;
+
+        public BabelDirectory(string path)
+        {
+            
+            uri = new Uri(WPath.GetFullPath(path.ToLower() + "\\"));
+            Path = Uri.UnescapeDataString(uri.AbsolutePath);
+        }
+
+        public string Path { get; }
+    }
+}
---
+++ BabelPath/BabelPathTests/Utilities/Path/BabelDirectoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Babel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel.Tests
+{
+    [TestClass()]
+    public class BabelDirectoryTests
+    {
+        [TestMethod()]
+        public void BabelDirectory_BabelDirectoryTest()
+        {
+            {
+                var path = new BabelDirectory("c:\\Temp");
+                Console.WriteLine(path.Path);
+            }
+            {
+                var path = new BabelDirectory("c:\\hoge\\..\\temp");
+                Console.WriteLine(path.Path);
+            }
+            {
+                var path = new BabelDirectory("c:\\hoge\\..\\temp\\");
+                Console.WriteLine(path.Path);
+            }
+        }
+    }
+}
